41941e85687d9ad8f36d415ca85aaccc1252b80c
syncpeterwhite <peter.white@synccomputing.com>
2022-07-05 19:53:57 -0400

    remove testing

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import Test3Calendar from "../components/Test3Calendar.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
-  },
-  {
-    path: "/test",
-    name: "test",
-    component: Test3Calendar,
   },
   {
     path: "/about",
